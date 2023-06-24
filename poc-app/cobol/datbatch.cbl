b4cff23ddbfe174c9aaa9dd83984b1e003bbdeaa
nelson <nelson.lopez1@ibm.com>
2023-06-24 16:14:07 -0400

    v0922i

--- poc-app/cobol/datbatch.cbl
+++ poc-app/cobol/datbatch.cbl
@@ -15,7 +15,7 @@
       * COPY DATSHARE.
       *
        PROCEDURE DIVISION.
-           DISPLAY 'DATBATCH DEMO az 0922h'
+           DISPLAY 'DATBATCH DEMO az 0922i'
       *
       *========= display copybook literals
       *     DISPLAY 'CPYBK=DATDEPND -> ' WS-VER.
