9f8a89dc51fa8d6a902e6ef8efec88a93fcea529
nelson lopez <nelson.lopez1@ibm.com>
2023-07-23 09:27:08 -0400

    reinit num1

--- poc-app/cobol/datbatch.cbl
+++ poc-app/cobol/datbatch.cbl
@@ -8,7 +8,7 @@
        WORKING-STORAGE SECTION.
        01 WK-AREA1.
           05  FILLER       PIC X(80).
-          05  num1         PIC 9(3) value is 001.
+          05  num1         PIC 9(3) value is 000.
           05  num2         PIC 9(3) value is 003.
       *
        COPY DATDEPND.
