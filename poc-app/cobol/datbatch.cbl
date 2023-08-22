3b6c22911796c9ed1ee28ad19fa5579a776f0722
nelson <nelson.lopez1@ibm.com>
2023-08-22 17:33:13 -0400

    cp37_test

--- poc-app/cobol/datbatch.cbl
+++ poc-app/cobol/datbatch.cbl
@@ -8,6 +8,7 @@
           05  FILLER       PIC X(80).
           05  num1         PIC 9(3) value is 000.
           05  num2         PIC 9(3) value is 003.
+          05  CHAR_A_CP37  PIC X(1) value 'A'.
       *
        COPY DATDEPND.
       *
