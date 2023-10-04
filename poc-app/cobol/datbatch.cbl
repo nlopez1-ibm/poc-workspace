338d9618a03bdc0d8bd18df8bc7bac90333d8b55
nelson lopez <nelson.lopez1@ibm.com>
2023-10-04 07:42:34 -0400

    test v22

--- poc-app/cobol/datbatch.cbl
+++ poc-app/cobol/datbatch.cbl
@@ -1,6 +1,6 @@
        ID DIVISION.
        PROGRAM-ID. DATBATCH.
-      * az test
+      * az test ++
       * Use Pub GITHUB project's DEVELOP branch
       * References STATIC MOD DEMOSUB in zdev.main.load for IAC Demos
        ENVIRONMENT DIVISION.
