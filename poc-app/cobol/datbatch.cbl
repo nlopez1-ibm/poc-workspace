0bd1f50a3cd7feb9c36c669dd2af1b26c74a8707
nelson <nelson.lopez1@ibm.com>
2023-11-05 11:27:09 -0500

    +++

--- poc-app/cobol/datbatch.cbl
+++ poc-app/cobol/datbatch.cbl
@@ -1,6 +1,6 @@
        ID DIVISION.
        PROGRAM-ID. DATBATCH.
-      * az test ++
+      * az test +++
       * Use Pub GITHUB project's DEVELOP branch test
       * References STATIC MOD DEMOSUB in zdev.main.load for IAC Demos
        ENVIRONMENT DIVISION.
@@ -9,12 +9,11 @@
        01 WK-AREA1.
           05  FILLER       PIC X(80).
           05  num1         PIC 9(3) value is 000.
-          05  num2         PIC 9(3) value is 003.     
+          05  num2         PIC 9(3) value is 003.
       *
        COPY DATDEPND.
       *
        PROCEDURE DIVISION.
            DISPLAY 'demo v.a.9'.
-          
+
            STOP RUN.
-    
