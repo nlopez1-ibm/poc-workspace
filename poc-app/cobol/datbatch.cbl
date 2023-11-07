bdc1e41727de3522ecd26fd651f84d7ae08979d0
nelson <nelson.lopez1@ibm.com>
2023-11-07 09:07:52 -0500

    v7

--- poc-app/cobol/datbatch.cbl
+++ poc-app/cobol/datbatch.cbl
@@ -1,6 +1,6 @@
        ID DIVISION.
        PROGRAM-ID. DATBATCH.
-      * TRACE drift v6
+      * TRACE drift v7
       * Use Pub GITHUB project's DEVELOP branch test
       * References STATIC MOD DEMOSUB in zdev.main.load for IAC Demos
        ENVIRONMENT DIVISION.
