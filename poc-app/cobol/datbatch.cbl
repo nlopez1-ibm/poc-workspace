19d468886f3109283453ecef0c0bb899a6b0375b
nelson lopez <nelson.lopez1@ibm.com>
2023-12-14 09:55:51 -0500

    demo v16

--- poc-app/cobol/datbatch.cbl
+++ poc-app/cobol/datbatch.cbl
@@ -14,7 +14,7 @@
        COPY DATDEPND.
       *
        PROCEDURE DIVISION.
-           DISPLAY 'DATBATCH: fix demo 15'.
+           DISPLAY 'DATBATCH: fix demo 16'.
            CALL 'DEMOSUB'.
 
            STOP RUN.
