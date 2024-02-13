fdb480131e437958a95a918261de68ccd5b2e477
nlopez <nelson.lopez1@ibm.com>
2024-02-13 08:04:08 -0500

    demo static in shared repos

--- poc-app/cobol/datbatch.cbl
+++ poc-app/cobol/datbatch.cbl
@@ -1,23 +1,25 @@
        ID DIVISION.
        PROGRAM-ID. DATBATCH.
-
-      * Use Pub GITHUB project's DEVELOP branch test
-      * References STATIC MOD DEMOSUB in zdev.main.load for IAC Demos
+      * Geneic Batch Cobol Demo with shared code examples.
        ENVIRONMENT DIVISION.
        DATA DIVISION.
        WORKING-STORAGE SECTION.
        01 WK-AREA1.
           05  FILLER       PIC X(80).
           05  num1         PIC 9(3) value is 000.
-          05  num2         PIC 9(3) value is 003.
-      *
        COPY DATDEPND.
+
       * Demo shared copybook 
        COPY DATSHARE.
        
       *
        PROCEDURE DIVISION.
            DISPLAY 'DATBATCH: new wass Image dec 2023 v5 '.
+
+      * Call a static sub in this repo     
            CALL 'DEMOSUB'.
+           
+      * An Static in another repo      
+           CALL STATIC1.
 
            STOP RUN.
