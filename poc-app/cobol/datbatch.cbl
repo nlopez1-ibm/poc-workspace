7ada7ae5f92b751ea40304a161b77836c9e3ecfc
nelson lopez <nelson.lopez1@ibm.com>
2024-02-16 10:08:43 -0500

    demo v2 jenk

--- poc-app/cobol/datbatch.cbl
+++ poc-app/cobol/datbatch.cbl
@@ -14,7 +14,7 @@
        
       * 
        PROCEDURE DIVISION.
-           DISPLAY 'DATBATCH: Main Line v12'.
+           DISPLAY 'DATBATCH: Main Line v2'.
            Display shared-f1.                                                  
       * Call a static sub in this repo     
            CALL 'DEMOSUB'.
