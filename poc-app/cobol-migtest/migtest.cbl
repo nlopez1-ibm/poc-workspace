f1489f70586f1d268bc939fd71909b667890f39d
nelson <nelson.lopez1@ibm.com>
2024-03-13 09:15:16 -0400

    migtest x41

---
+++ poc-app/cobol-migtest/migtest.cbl
@@ -0,0 +1,11 @@
+       ID DIVISION.
+       PROGRAM-ID. MIGTEST.
+      *
+      * test tcs mig x41 in a commnet line
+      * BADLINE <LABEL:ACE-14> 123456789
+       ENVIRONMENT DIVISION.
+       DATA DIVISION.
+       WORKING-STORAGE SECTION.
+       PROCEDURE DIVISION.
+           DISPLAY 'MIGTEST here'.
+           STOP RUN.
