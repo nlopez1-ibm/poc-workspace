f464b560005b915f18528466629296c88dc9b945
nlopez <nelson.lopez1@ibm.com>
2024-03-22 15:22:33 -0400

    test static reset

--- poc-app/cobol/datbatch.cbl
+++ poc-app/cobol/datbatch.cbl
@@ -14,7 +14,7 @@
        
       *
        PROCEDURE DIVISION.
-           DISPLAY 'DATBATCH: Main Audit test v9'.
+           DISPLAY 'DATBATCH: Main pgm test'.s
            Display shared-f1.                                                  
       * Call a static sub in this repo  
       * disabale all static calls to test dep  in src scan phase    
